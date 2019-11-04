21dc72a44134c2def6c43cd9a329ec2c38bb43a2
admin <admin@admins-MacBook-Pro.local>
2019-11-04 13:18:02 -0800

    mvp complete

--- accounts/accountRouter.js
+++ accounts/accountRouter.js
@@ -23,16 +23,22 @@ router.get('/:id', (req, res)=> {
      .then(response => {
           res.status(200).json(response)
      })
+     .catch(error => {
+          res.status(500).json({error: 'Failed to get account from database'})
+     })
 })
 
 
 // TODO: POST new account + authenticate data sent by client
      // requires name + budget 
-router.post('/', (req, res) => {
+router.post('/', validatePost, (req, res) => {
      knex.insert(req.body, 'id')
      .into('accounts')
      .then(response => {
           res.status(200).json(response)
+     })
+     .catch(error => {
+          res.status(500).json({error: 'Failed post new account'})
      })
 })
 
@@ -45,11 +51,14 @@ router.delete('/:id', (req, res) => {
      .then(response => {
           res.status(200).json(response)
      })
+     .catch(error => {
+          res.status(500).json({error: 'Failed to delete account'})
+     })
 })
 
 
 // TODO: UPDATE account + validate data
-router.put('/:id', (req, res) => {
+router.put('/:id', validatePost, (req, res) => {
      const changes = req.body
 
      knex('accounts')
@@ -58,22 +67,23 @@ router.put('/:id', (req, res) => {
      .then(response => {
           res.status(200).json(response)
      })
+     .catch(error => {
+          res.status(500).json({error: 'Failed to update account'})
+     })
 })
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// TODO Middleware authentication
+function validatePost(req, res, next){
+     if(req.body){
+          if(req.body.name && req.body.budget){
+               next()
+          }
+          else{ res.status(404).json({error: "Error: Missing name or budget input"})}
+     } else{
+          res.status(500).json({error: "Invalid account entry"})
+     }
+}
 
 module.exports = router;
